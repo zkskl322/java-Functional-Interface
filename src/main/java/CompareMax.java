1c2acbec5e3df08673610fc06c39cacfbb084a61
zkskl322 <dbawo530@gmail.com>
2024-07-18 17:31:37 +0900

    java-Functional-Interface

---
+++ src/main/java/CompareMax.java
@@ -0,0 +1,5 @@
+@FunctionalInterface
+public interface CompareMax {
+
+    int max(int x, int y);
+}
---
+++ src/main/java/CompareMaxMain02.java
@@ -0,0 +1,20 @@
+public class CompareMaxMain02 {
+    public static void main(String[] args) {
+
+        int num01 = 20;
+        int num02 = 30;
+
+        // 익명클래스 만들기
+        CompareMax compareMax = (x, y) -> {return x > y ? 1 : x < y ? -1 : 0;};
+
+        int result = compareMax.max(num01, num02);
+
+        if(result == 1) {
+            System.out.println(num01 + " 이 " + num02 + " 보다 크다");
+        }else if(result == -1) {
+            System.out.println(num01 + " 이 " + num02 + " 보다 작다");
+        }else {
+            System.out.println(num01 + " 과 " + num02 + " 는 같다");
+        }
+    }
+}
